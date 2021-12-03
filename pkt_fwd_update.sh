c2daf05c6e7e6d7716788e7f52bd78f3c71d874b
greenward <2272175+greenward@users.noreply.github.com>
2021-12-03 19:24:41 +0000

    Update pkt_fwd_update.sh

--- pkt_fwd_update.sh
+++ pkt_fwd_update.sh
@@ -4,7 +4,7 @@ wget --no-cache https://raw.githubusercontent.com/greenward/PiscesQoLDashboard/m
 
 tar -xzf /tmp/green_packet_v2.0.1.tgz -C /home/pi/hnt/paket
 cp /home/pi/api/tool/onPacket.sh /home/pi/api/tool/onPacket.sh.orig
-sed -i sed 's|/home/pi/hnt/paket/paket/packet_forwarder/|/home/pi/hnt/paket/green_packet/|' /home/pi/api/tool/onPacket.sh
+sed -i 's|/home/pi/hnt/paket/paket/packet_forwarder/|/home/pi/hnt/paket/green_packet/|' /home/pi/api/tool/onPacket.sh
 kill $(ps ax | grep './lora_pkt_fwd' | grep -v 'grep' | awk '{print $1}')
 
 bash /home/pi/api/tool/onPacket.sh > /dev/null 2>&1
